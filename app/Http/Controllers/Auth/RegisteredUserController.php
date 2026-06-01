b5d682ec98725cd598c2864287d4e4645c52bff5
caderammar0708 <caderammar2006@gmail.com>
2026-06-01 14:44:57 +0530

    Changed to laravel 12

---
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Display the registration view.
+     */
+    public function create(): Response
+    {
+        return Inertia::render('Auth/Register', [
+            'canLogin' => Route::has('login'),
+        ]);
+    }
+
+    /**
+     * Handle an incoming registration request.
+     */
+    public function store(RegisterRequest $request): RedirectResponse
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+        ]);
+
+        event(new Registered($user));
+
+        Auth::login($user);
+
+        return redirect(RouteServiceProvider::HOME);
+    }
+}
